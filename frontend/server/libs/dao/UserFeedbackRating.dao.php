7580e92c6bef08364bba4ad6bdf4b6d1d36582d7
Joemmanuel <joemmanuel@gmail.com>
2016-09-10 19:42:52 +0000

    UserFeedbackRating DAOs

---
+++ frontend/server/libs/dao/UserFeedbackRating.dao.php
@@ -0,0 +1,14 @@
+<?php
+
+include('base/UserFeedbackRating.dao.base.php');
+include('base/UserFeedbackRating.vo.base.php');
+/** UserFeedbackRating Data Access Object (DAO).
+  *
+  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para
+  * almacenar de forma permanente y recuperar instancias de objetos {@link UserFeedbackRating }.
+  * @access public
+  *
+  */
+class UserFeedbackRatingDAO extends UserFeedbackRatingDAOBase
+{
+}
---
+++ frontend/server/libs/dao/base/UserFeedbackRating.vo.base.php
@@ -0,0 +1,129 @@
+<?php
+
+/** ******************************************************************************* *
+  *                    !ATENCION!                                                   *
+  *                                                                                 *
+  * Este codigo es generado automaticamente. Si lo modificas tus cambios seran      *
+  * reemplazados la proxima vez que se autogenere el codigo.                        *
+  *                                                                                 *
+  * ******************************************************************************* */
+
+/** Value Object file for table UserFeedbackRating.
+  * 
+  * VO does not have any behaviour.
+  * @access public
+  * 
+  */
+
+class UserFeedbackRating extends VO
+{
+	/**
+	  * Constructor de UserFeedbackRating
+	  * 
+	  * Para construir un objeto de tipo UserFeedbackRating debera llamarse a el constructor 
+	  * sin parametros. Es posible, construir un objeto pasando como parametro un arreglo asociativo 
+	  * cuyos campos son iguales a las variables que constituyen a este objeto.
+	  */
+	function __construct($data = NULL)
+	{
+		if (isset($data))
+		{
+			if (is_string($data))
+				$data = self::object_to_array(json_decode($data));
+
+
+			if (isset($data['rating_id'])) {
+				$this->rating_id = $data['rating_id'];
+			}
+			if (isset($data['user_id'])) {
+				$this->user_id = $data['user_id'];
+			}
+			if (isset($data['problem_id'])) {
+				$this->problem_id = $data['problem_id'];
+			}
+			if (isset($data['contest_id'])) {
+				$this->contest_id = $data['contest_id'];
+			}
+			if (isset($data['create_date'])) {
+				$this->create_date = $data['create_date'];
+			}
+			if (isset($data['raiting'])) {
+				$this->raiting = $data['raiting'];
+			}
+		}
+	}
+
+	/**
+	  * Obtener una representacion en String
+	  * 
+	  * Este metodo permite tratar a un objeto UserFeedbackRating en forma de cadena.
+	  * La representacion de este objeto en cadena es la forma JSON (JavaScript Object Notation) para este objeto.
+	  * @return String 
+	  */
+	public function __toString( )
+	{ 
+		$vec = array( 
+			"rating_id" => $this->rating_id,
+			"user_id" => $this->user_id,
+			"problem_id" => $this->problem_id,
+			"contest_id" => $this->contest_id,
+			"create_date" => $this->create_date,
+			"raiting" => $this->raiting
+		); 
+	return json_encode($vec); 
+	}
+
+	/**
+	 * Converts date fields to timestamps
+	 **/
+	public function toUnixTime(array $fields = array()) {
+		if (count($fields) > 0)
+			parent::toUnixTime($fields);
+		else
+			parent::toUnixTime(array("create_date"));
+	}
+
+	/**
+	  *  [Campo no documentado]
+	  * Llave Primaria
+	  * Auto Incremento
+	  * @access public
+	  * @var int(11)
+	  */
+	public $rating_id;
+
+	/**
+	  *  [Campo no documentado]
+	  * @access public
+	  * @var int(11)
+	  */
+	public $user_id;
+
+	/**
+	  *  [Campo no documentado]
+	  * @access public
+	  * @var int(11)
+	  */
+	public $problem_id;
+
+	/**
+	  *  [Campo no documentado]
+	  * @access public
+	  * @var int(11)
+	  */
+	public $contest_id;
+
+	/**
+	  *  [Campo no documentado]
+	  * @access public
+	  * @var timestamp
+	  */
+	public $create_date;
+
+	/**
+	  *  [Campo no documentado]
+	  * @access public
+	  * @var tinyint
+	  */
+	public $raiting;
+}
