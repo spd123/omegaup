2ca687f811e4f666daeb6e99946c35ab8eb2ffa8
Joemmanuel <joemmanuel@gmail.com>
2016-09-10 19:36:44 +0000

    UserFeedbackRating schema

---
+++ frontend/database/8_user_feedback_rating.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `UserFeedbackRating` (
+		`rating_id` int(11) NOT NULL AUTO_INCREMENT,
+		`user_id` int(11) NOT NULL,
+		`problem_id` int(11) NULL,
+		`contest_id` int(11) NULL,
+		`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		`raiting` tinyint UNSIGNED NOT NULL,
+		PRIMARY KEY (`rating_id`),
+		KEY idx_rating_problem (`user_id`, `problem_id`),
+		KEY idx_rating_contest (`user_id`, `contest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Guarda raitings de usuarios a objetos de omegaUp' AUTO_INCREMENT=1 ;
+
+ALTER TABLE `UserFeedbackRating`
+  ADD CONSTRAINT `fk_ufr_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `UserFeedbackRating`
+	ADD CONSTRAINT `fk_ufr_problem_id` FOREIGN KEY (`problem_id`) REFERENCES `Problems` (`problem_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `UserFeedbackRating`
+	ADD CONSTRAINT `fk_ufr_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -766,6 +766,21 @@ CREATE TABLE IF NOT EXISTS `Run_Counts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Guarda la cantidad de runs que se han realizado hasta la fecha.';
 
 --
+-- Estructura de tabla para `UserFeedbackRating`
+--
+CREATE TABLE IF NOT EXISTS `UserFeedbackRating` (
+		`rating_id` int(11) NOT NULL AUTO_INCREMENT,
+		`user_id` int(11) NOT NULL,
+		`problem_id` int(11) NULL,
+		`contest_id` int(11) NULL,
+		`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		`raiting` tinyint UNSIGNED NOT NULL,
+		PRIMARY KEY (`rating_id`),
+		KEY idx_rating_problem (`user_id`, `problem_id`),
+		KEY idx_rating_contest (`user_id`, `contest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Guarda raitings de usuarios a objetos de omegaUp' AUTO_INCREMENT=1 ;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -997,6 +1012,15 @@ ALTER TABLE `Groups_Scoreboards`
 ALTER TABLE `Groups_Scoreboards_Contests`
   ADD CONSTRAINT `fk_gsc_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_gsc_group_scoreboard_id` FOREIGN KEY (`group_scoreboard_id`) REFERENCES `Groups_Scoreboards` (`group_scoreboard_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `UserFeedbackRating`
+  ADD CONSTRAINT `fk_ufr_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `UserFeedbackRating`
+	ADD CONSTRAINT `fk_ufr_problem_id` FOREIGN KEY (`problem_id`) REFERENCES `Problems` (`problem_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `UserFeedbackRating`
+	ADD CONSTRAINT `fk_ufr_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Update AC Count on grade
